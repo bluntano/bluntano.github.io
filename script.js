19ba210f8c3d309732f2c280ac579379d3a4ec5c
bluntano <electric.lazer99@gmail.com>
2020-08-22 15:07:24 +0300

    Added new funky stuff
    innit

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log('welcome to matrix');
+let msgBox = document.getElementById('msgbox');
+
+console.log('** welcome to matrix **');
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
 
 var app = new Vue({
     el: '#app',
@@ -17,6 +24,38 @@ var app2 = new Vue({
 var app3 = new Vue({
     el: '#app3',
     data: {
-        message: 'Type something really funny!'
+        message: 'Type something really funny!',
+        typing: false
     }
-})
+});
+
+var app4 = new Vue({
+    el: '#app4',
+    data: {
+        todos: [
+            { id: 0, text: 'learn Vue.js'},
+            { id: 1, text: 'finish this stupid military service'}
+        ]
+    }
+});
+
+function checkMsgBoxEnterKey(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        let content = document.querySelector('#msgbox').value;
+        app4.todos.push({ text: content });
+    };
+};
+
+let typingTimeout;
+msgBox.onkeypress = () => {
+    if (typingTimeout != undefined) {
+        clearTimeout(typingTimeout);
+        app3.typing = true;
+    };
+    typingTimeout = setTimeout(isTyping, 250);
+};
+
+function isTyping() {
+    return app3.typing = false;
+};
